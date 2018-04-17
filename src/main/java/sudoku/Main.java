1e2d00ab13f3947d2a2117727c94ecf176dfe51f
milad <milad.f@mail.com>
2018-04-17 17:17:22 -0400

    Add the Main class and create a basic menu
    - Also set the main class in maven jar plugin to
      be able to run the jar file
    
    - Enforce maven to build the project with java 1.8

---
+++ src/main/java/sudoku/Main.java
@@ -0,0 +1,38 @@
+package sudoku;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		boolean quit = false;
+		while (!quit) {
+			System.out.println("\nEnter action: (9 to show available actions)");
+			String action = scanner.nextLine();
+			switch (action) {
+			case "1":
+				System.out.println("\nStart the Game ");
+				System.out.println("------------------------");
+				break;
+			case "2":
+				System.out.println("\nThe Answer");
+				System.out.println("------------------------\n");
+				break;
+			case "8":
+				System.out.println("\nGood bye");
+				quit = true;
+				break;
+			default:
+				StringBuilder msg = new StringBuilder("\nAvailable actions:\n");
+				msg.append("------------------------\n");
+				msg.append("1- New Game\n");
+				msg.append("2- Show solution\n");
+				msg.append("8- Quit\n");
+				System.out.println(msg);
+			}
+		}
+	}
+}
